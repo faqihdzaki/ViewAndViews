96aa16e034e935b0248e29ee1bfc5896d8935f94
faqihdzaki <faqihdzaki11@gmail.com>
2019-09-28 20:47:14 +0700

    viewandViews Tugas 2

---
+++ app/src/main/java/faqih/nat/myviewandviews/Donasi.java
@@ -0,0 +1,14 @@
+package faqih.nat.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Donasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donasi);
+    }
+}
---
+++ app/src/main/java/faqih/nat/myviewandviews/MainActivity.java
@@ -0,0 +1,34 @@
+package faqih.nat.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar()!= null){
+            getSupportActionBar().setTitle("Faqih Ganteng");
+
+            Button btndonasi =findViewById(R.id.btn_donasi);
+            btndonasi.setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_donasi :
+                Intent moveIntent = new Intent(MainActivity.this,Donasi.class);
+                startActivity(moveIntent);
+                break;
+        }
+    }
+}
